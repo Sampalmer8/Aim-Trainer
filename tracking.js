5883d8ae8b0f868f6336104c994534e29eb34658
Sampalmer8 <sampalmerg@gmail.com>
2022-11-07 11:23:17 -0700

    tracking done

--- tracking.js
+++ tracking.js
@@ -1,22 +1,22 @@
-// window.addEventListener("load", draw);
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+// Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
-// // Canvas Setup
-// let cnv = document.getElementById("myCanvas");
-// let ctx = cnv.getContext("2d");
-// cnv.width = 1000;
-// cnv.height = 1000;
-// x = 500;
-// y = 500;
-
-// // Movement
-// function draw() {
-//     // Circle
-//     ctx.fillStyle = "rgb(0, 255, 255)";
-//     ctx.beginPath();
-//     ctx.arc(x, y, 25, 0, 2 * Math.PI);
-//     ctx.fill();
-//     requestAnimationFrame(draw);
-// }
+// Movement
 
 "use strict";
 
@@ -49,8 +49,8 @@ let time;
 let position = new Vector(500, 500); // Pixels
 let start = new Vector(Infinity, Infinity); // Pixels
 let direction = new Vector(0, 0); // Pixels
-let speed = 80.0; // Pixels per second
-let step = 50.0; // Pixels
+let speed = 85.0; // Pixels per second
+let step = 60.0; // Pixels
 
 function tick(newTime){
     let delta = (newTime - time) / 1000.0;
@@ -78,6 +78,21 @@ function setup() {
 }
 
 function drawBall(position) {
+    if (position.x < 0) {
+        position.x = 500;
+    }
+
+    if (position.y < 0) {
+        position.y = 500;
+    }
+
+    if (position.x > canvas.width) {
+        position.x = 500;
+    }
+
+    if (position.y > canvas.width) {
+        position.y = 500;
+    }
     ctx.save();
     {
         ctx.translate(position.x, position.y);
